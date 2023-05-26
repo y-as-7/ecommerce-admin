4d04244370d1c83aaa65192422d955fb7d8d1f52
youssef askar <youssefaskar111@outlook.com>
2023-05-26 14:26:05 +0300

    finish updata well

--- pages/api/products.js
+++ pages/api/products.js
@@ -18,12 +18,16 @@ export default async function handle(req, res) {
   if (method === "POST") {
     const { title, description, price } = req.body;
     const productDoc = await Product.create({
-      title,
-      description,
-      price,
+        title,
+        description,
+        price,
     });
     res.json(productDoc);
-  }
+}
 
-  
+if (method==='PUT') {
+      const { title, description, price ,_id} = req.body;
+  await  Product.updateOne({_id},{title,description,price})
+  }
+  res.json(true)
 }
--- pages/components/ProductForm.jsx
+++ pages/components/ProductForm.jsx
@@ -1,51 +1,60 @@
-import axios from 'axios';
-import { useRouter } from 'next/router';
-import React, { useState } from 'react';
+import axios from "axios";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
-const ProductForm = ({title:existingTitle,description:existingDescription, price:existingPrice}) => {
-    const [title, setTitle] = useState(existingTitle||'');
-    const [description, setDescription] = useState(existingDescription||'');
-    const [price, setPrice] = useState(existingPrice||'');
-    const[goToProducts,setGoToproducts]=useState(false)
-      const  router=useRouter();
-   async function createProduct  (ev){
-       ev.preventDefault();
-       const data = {title,description,price};
-       await axios.post('/api/products',data);
-       setGoToproducts(true);
-       
-      }
-      if (goToProducts) {
-         router.push('/products');
-      }
-    return (
-     
-        <form onSubmit={createProduct}>
-        
-          <label>Product name</label>
-          <input
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            type="text"
-            placeholder="product name"
-          />
-          <label>Product description</label>
-          <textarea
-            onChange={(e) => setDescription(e.target.value)}
-            value={description}
-            placeholder="description"
-          ></textarea>
-          <label>Price </label>
-          <input
-            onChange={(e) => setPrice(e.target.value)}
-            value={price}
-            type="number"
-            placeholder="price"
-          />
-          <button className="btn-primary">Save</button>
-        </form>
-      
-    );
-}
+const ProductForm = ({
+  _id,
+  title: existingTitle,
+  description: existingDescription,
+  price: existingPrice,
+}) => {
+  const [title, setTitle] = useState(existingTitle || "");
+  const [description, setDescription] = useState(existingDescription || "");
+  const [price, setPrice] = useState(existingPrice || "");
+  const [goToProducts, setGoToproducts] = useState(false);
+  const router = useRouter();
+
+  async function createProduct(ev) {
+    ev.preventDefault();
+    const data = { title, description, price };
+    if (_id) {
+      // update
+      await axios.put("/api/products", { ...data, _id });
+    } else {
+      //create
+
+      await axios.post("/api/products", data);
+    }
+    setGoToproducts(true);
+  }
+  if (goToProducts) {
+    router.push("/products");
+  }
+  return (
+    <form onSubmit={createProduct}>
+      <label>Product name</label>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        type="text"
+        placeholder="product name"
+      />
+      <label>Product description</label>
+      <textarea
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+        placeholder="description"
+      ></textarea>
+      <label>Price </label>
+      <input
+        onChange={(e) => setPrice(e.target.value)}
+        value={price}
+        type="number"
+        placeholder="price"
+      />
+      <button className="btn-primary">Save</button>
+    </form>
+  );
+};
 
 export default ProductForm;
